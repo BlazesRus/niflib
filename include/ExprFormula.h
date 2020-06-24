8bab4ac367c21de37538bd7062536b84ebc20627
James Michael Armstrong(BlazesRus) <BlazesRus@yahoo.com>
2020-06-23 23:14:33 -0400

    More code and layout added for ExprFormula

--- include/ExprFormula.h
+++ include/ExprFormula.h
@@ -23,17 +23,59 @@ private:
     /// </summary>
     ExprFormula() {}
 public:
+    /// <summary>
+    /// Class FormulaElement. 
+    /// Implements the <see cref="std::vector{std::string}" />
+    /// </summary>
+    /// <seealso cref="std::vector{std::string}" />
     NIFLIB_API class FormulaElement : std::vector<std::string>
-    {
+    {//@; #i, and #f starting elements reserved, and of course all operator values
+       
+        // Expression Example: ((Variable01 >= 1)&&(Variable02>2.5))
+        // with (Variable01 >= 1) FormulaElement values:
+        // Variable01, >=, #i0
+        // with (Variable01 >= 1) values referenced from:
+        // Variable01, >=, IntValues[0]
+        // with (Variable02>2.5) FormulaElement values:
+        // Variable02, >, #i0
+        // with (Variable02>2.5) values referenced from:
+        // Variable02, >, FloatValues[0]
+        // Initial FormulaElement Values : @1, &&, @2
+
     };
     std::vector<FormulaElement> ExprLayout;
-    std::vector<std::string> ValueNames;
+
+    //Don't need to refer to element for variable names
+    //std::vector<std::string> ValueNames;
+
+    //Might add optional vector reference to other values stored elsewhere for evaluating values
+
+    /// <summary>
+    /// The stored int values
+    /// </summary>
+    std::vector<int> IntValues;
+    /// <summary>
+    /// The stored float values
+    /// </summary>
+    std::vector<int> FloatValues;
+
+    bool EvalAsBool()
+    {
+        return true;//Placeholder code
+    }
+
     /// <summary>
     /// Initializes a new instance of the <see cref="ExprFormula"/> class.
     /// </summary>
     /// <param name="ElemValue">The elem value to read in order to create formula data.</param>
     ExprFormula(std::string ElemValue)
     {
-
+        //0 = ???
+        //1 = Number
+        //2 = Operator
+        //3 = Value
+        short ScanType = 0;
+        size_t CurrentFormElement = 0;
+        ExprLayout.push_back(FormulaElement());
     }
 };
